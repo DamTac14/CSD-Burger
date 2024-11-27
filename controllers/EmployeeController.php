5119f642035056aee2f99824abb7cd3f2c255b79
sofcomlink <sofiane.kaya@comlink.fr>
2024-11-27 12:25:26 +0100

    deplacement des fichiers html dans views

--- controllers/EmployeeController.php
+++ controllers/EmployeeController.php
@@ -9,7 +9,7 @@ class EmployeeController {
         $this->pdo = $pdo;
     }
 
-    public function addEmployee($role, $login, $password, $is_active, $hire_date, $departure_date) {
+    public function addEmployee($firs_tname, $last_name, $role, $login, $password, $is_active, $hire_date, $departure_date) {
         $sql = "INSERT INTO employees (role, login, password, is_active, hire_date, departure_date) 
                 VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $this->pdo->prepare($sql);
