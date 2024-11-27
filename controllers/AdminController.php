f7eaf6f8ee7162e76737d12e6134f7d8356bd066
sofcomlink <sofiane.kaya@comlink.fr>
2024-11-27 15:58:58 +0100

    auth controller

---
+++ controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php  
+
+class AuthController {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function login($userName, $password) {
+        $sql = "SELECT * FROM employees WHERE user_name = ?";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$userName]);
+        $employee = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($employee && password_verify($password, $employee['password'])) {
+            return $employee; 
+        }
+        return false;
+    }
+
+    public function logout() {
+        session_destroy();
+        unset($_SESSION['id']);
+        return true;
+    }
+}
+
