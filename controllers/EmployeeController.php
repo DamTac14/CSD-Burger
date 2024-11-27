fe4bcda55572f4712d9650e751554e6dc8505677
sofcomlink <sofiane.kaya@comlink.fr>
2024-11-27 12:56:11 +0100

    menus controller

--- controllers/EmployeeController.php
+++ controllers/EmployeeController.php
@@ -9,12 +9,12 @@ class EmployeeController {
         $this->pdo = $pdo;
     }
 
-    public function addEmployee($firs_tname, $last_name, $role, $login, $password, $is_active, $hire_date, $departure_date) {
-        $sql = "INSERT INTO employees (role, login, password, is_active, hire_date, departure_date) 
+    public function addEmployee($first_name, $last_name, $user_name, $role, $password, $is_active, $hire_date, $departure_date) {
+        $sql = "INSERT INTO employees (first_name, last_name, user_name, role, password, is_active, hire_date, departure_date) 
                 VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $this->pdo->prepare($sql);
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $stmt->execute([$role, $login, $hashedPassword, $is_active, $hire_date, $departure_date]);
+        $stmt->execute([$first_name, $last_name, $user_name, $role, $hashedPassword, $is_active, $hire_date, $departure_date]);
         return true;
     }
 
@@ -26,13 +26,13 @@ class EmployeeController {
         return $employees;
     }
 
-    public function updateEmployee($id, $role, $password, $is_active, $hire_date, $departure_date) {
+    public function updateEmployee($id, $last_name, $first_name, $user_name, $role, $password, $is_active, $hire_date, $departure_date) {
         $sql = "UPDATE employees 
-                SET role = ?, password = ?, is_active = ?, hire_date = ?, departure_date = ?
+                SET  first_name = ?, last_name = ?, user_name = ?, role = ?, password = ?, is_active = ?, hire_date = ?, departure_date = ?
                 WHERE id = ?";
         $stmt = $this->pdo->prepare($sql);
         $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $result = $stmt->execute([$role, $hashedPassword, $is_active, $hire_date, $departure_date, $id]);
+        $result = $stmt->execute([$first_name, $last_name, $user_name, $role, $hashedPassword, $is_active, $hire_date, $departure_date, $id]);
         return $result; 
     }
 
---
+++ controllers/MenuController.php
@@ -0,0 +1,40 @@
+<?php  
+
+class MenuController {
+    private $pdo;
+    
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function addMenu($name, $image, $includeDishes) {
+        $sql = "INSERT INTO menus (name, image, includeDishes) 
+                VALUES (?, ?, ?)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([$name, $image, $includeDishes]);
+        return true;
+    }
+
+    public function showMenu() {
+        $sql = "SELECT * FROM menus";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $menus = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $menus;
+    }
+
+    public function updateMenu($id, $name, $image, $includeDishes) {
+        $sql = "UPDATE menus 
+                SET name = ?, image = ?, includeDishes = ?
+                WHERE id = ?";
+        $stmt = $this->pdo->prepare($sql);
+        $result = $stmt->execute([$name, $image, $includeDishes, $id]);
+        return $result; 
+    }
+
+    public function deleteMenu($id) {
+        $sql = "DELETE FROM menus WHERE id = ?";
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+}
