dc402b16a5c0672a24a02fc619dcb28fe3fcdc96
FrozenCell239 <val8ioks@gmail.com>
2024-11-28 21:54:49 +0100

    Project fix - Added a missing option in the header.

--- views/header.php
+++ views/header.php
@@ -5,5 +5,6 @@
         <a href="stock-management.php">Gestion des stocks</a>
         <a href="insight-data.php">Rapport de performance</a>
         <a href="commandes.php">Tableau des commandes</a>
+        <a href="employee-management.php">Liste des employés</a>
         <a href="">Déconnexion</a>
     </header>
