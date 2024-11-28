7ab23fee71763e51484c0a666613946029779142
FrozenCell239 <val8ioks@gmail.com>
2024-11-28 21:31:44 +0100

    Error fix - Moved the employee CSS code to its own CSS file. - Fixed the name of employee.css file.

--- views/employee-management.php
+++ views/employee-management.php
@@ -15,53 +15,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="ressource/assets/header.css">
-        <style>
-            table{
-                border-radius: 25px;
-                width: 100%;
-                text-align: center;
-                border-spacing: 0;
-                font-size: larger;
-            }
-            thead{
-                background-color: #546C98;
-            }
-            tbody{
-                color: #222222;
-                background-color: #F5F5F5;
-            }
-            tr:nth-child(even){
-                background-color: #ADC7F3;
-                opacity: 40%;
-            }
-            th, td{
-                height: 50px;
-            }
-            #add-employee-button{
-                position: absolute;
-                bottom: 20px;
-                right: 50%;
-                left: auto;
-                background-color: #546C98;
-                color: white;
-                border-radius: 25px;
-                border: 0;
-                padding: 4px 20px;
-                font-size: 4rem;
-                cursor: pointer;
-                text-decoration: none;
-            }
-            #add-employee-button:hover{
-                background-color: #435984;
-            }
-        </style>
+        <link rel="stylesheet" href="ressource/assets/employee.css">
         <title>Gestion des équipiers</title>
     </head>
     <body>
         <?php include_once('./header.php'); ?>
         <main>
             <h1>Gestion des équipiers</h1>
-            <table>
+            <table id="employee-table" class="table">
                 <thead>
                     <tr>
                         <th style="border-top-left-radius: 25px;">Prénom NOM</th>
