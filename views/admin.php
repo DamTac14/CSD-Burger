de9ab42de07b3b499b08c675e5325c24720b62a6
sofcomlink <sofiane.kaya@comlink.fr>
2024-11-28 11:31:16 +0100

    menu administrateur

---
+++ views/admin.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administration</title>
+    <link rel="stylesheet" href="ressource/assets/admin.css"> 
+
+
+    <header>
+    <img src="ressource/images/logo.png" alt="logo-burger">
+        <a href="">Création des formules</a>
+        <a href="">Liste des formules</a>
+        <a href="">Déconnexion</a>
+    </header>
+</head>
+<body>
+    
+</body>
+</html>
