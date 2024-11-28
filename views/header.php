cf12f1c0e8e4f5ab125d1045259c987cf53912b8
FrozenCell239 <val8ioks@gmail.com>
2024-11-28 21:58:38 +0100

    Minor fix

--- views/header.php
+++ views/header.php
@@ -1,10 +1,10 @@
 <header>
     <img src="ressource/images/logo.png" alt="logo-burger">
-        <a href="create-menu.php">Création des formules</a>
-        <a href="menu-list.php">Liste des formules</a>
-        <a href="stock-management.php">Gestion des stocks</a>
-        <a href="insight-data.php">Rapport de performance</a>
-        <a href="commandes.php">Tableau des commandes</a>
-        <a href="employee-management.php">Liste des employés</a>
-        <a href="">Déconnexion</a>
-    </header>
+    <a href="create-menu.php">Création des formules</a>
+    <a href="menu-list.php">Liste des formules</a>
+    <a href="stock-management.php">Gestion des stocks</a>
+    <a href="insight-data.php">Rapport de performance</a>
+    <a href="commandes.php">Tableau des commandes</a>
+    <a href="employee-management.php">Liste des employés</a>
+    <a href="">Déconnexion</a>
+</header>
